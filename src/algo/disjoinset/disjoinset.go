26f5a463f5a5a0d73483b87f83135cc206ee15d3
a.kukuev <a.kukuev@corp.mail.ru>
2018-06-02 11:51:39 +0300

    impl disjoin set and test

--- src/algo/disjoinset/disjoinset.go
+++ src/algo/disjoinset/disjoinset.go
@@ -1,1 +1,46 @@
 package disjoinset
+
+func NewDisJoinSet() DisJoinSet {
+	s := DisJoinSet{}
+	s.parent = map[int]int{}
+	s.rank = map[int]int{}
+	return s
+}
+
+type DisJoinSet struct {
+	parent map[int]int
+	rank map[int]int // there only root, dont forgot delete on union less, and update new root
+}
+
+func (s *DisJoinSet) MakeSet(idx int) {
+	// TODO: check that not exist
+	s.parent[idx] = idx
+	s.rank[idx] = 0
+}
+
+func (s *DisJoinSet) Union(left, right int) {
+	leftParent := s.Find(left)
+	rightParent := s.Find(right)
+	if leftParent == rightParent {
+		// TODO: may be error?
+		return
+	}
+	if s.rank[leftParent] > s.rank[rightParent] {
+		s.parent[rightParent] = leftParent
+		delete(s.rank, rightParent)
+	} else {
+		s.parent[leftParent] = rightParent
+		delete(s.rank, leftParent)
+		if s.rank[leftParent] == s.rank[rightParent] {
+			s.rank[rightParent] += 1
+		}
+	}
+}
+
+func (s *DisJoinSet) Find(idx int) int {
+	if idx != s.parent[idx] {
+		s.parent[idx] = s.Find(s.parent[idx])
+	}
+
+	return s.parent[idx]
+}
--- src/algo/disjoinset/disjoinset_test.go
+++ src/algo/disjoinset/disjoinset_test.go
@@ -1,1 +1,59 @@
 package disjoinset
+
+import "testing"
+
+func TestJoinSets(t *testing.T) {
+	set1 := []int{1, 2, 3}
+	set2 := []int{4, 5, 6}
+
+	s := NewDisJoinSet()
+
+	for i := 1; i <= 6; i++ {
+		s.MakeSet(i)
+	}
+
+	for _, i := range set1 {
+		s.Union(set1[0], i)
+	}
+
+	for _, i := range set2 {
+		s.Union(set2[0], i)
+	}
+
+	// check that set1 is one set and set2 not in set1
+	for _, i := range set1 {
+		if s.Find(set1[0]) != s.Find(i) {
+			t.Errorf("[%v] not in set %v", i, set1)
+		}
+
+		if s.Find(set2[0]) == s.Find(i) {
+			t.Errorf("[%v] in set %v", i, set2)
+		}
+	}
+
+	for _, i := range set2 {
+		if s.Find(set2[0]) != s.Find(i) {
+			t.Errorf("[%v] not in set %v", i, set2)
+		}
+
+		if s.Find(set1[0]) == s.Find(i) {
+			t.Errorf("[%v] in set %v", i, set1)
+		}
+	}
+
+	s.Union(5, 2)
+
+	// check that all items in union set
+	for _, i := range set1 {
+		if s.Find(set1[0]) != s.Find(i) {
+			t.Errorf("[%v] not in set %v union %v", i, set1, set2)
+		}
+	}
+
+	for _, i := range set2 {
+		if s.Find(set1[0]) != s.Find(i) {
+			t.Errorf("[%v] not in set %v union %v", i, set1, set2)
+		}
+	}
+
+}
