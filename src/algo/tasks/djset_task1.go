91cbe05d80d9af42683a3713c41c512a25194ad5
a.kukuev <a.kukuev@corp.mail.ru>
2018-06-03 11:28:55 +0300

    impl task1 for dj set

--- src/algo/tasks/djset_task1.go
+++ src/algo/tasks/djset_task1.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"algo/disjoinset"
+	"fmt"
+)
+
+func main() {
+	var n, m int
+	fmt.Scanf("%v %v", &n, &m)
+
+	weights := make(map[int]int, n)
+	djset := disjoinset.NewDisJoinSet()
+	maxWeight := 0
+
+	for i := 1; i <= n; i++ {
+		var w int
+		fmt.Scanf("%v", &w)
+		weights[i] = w
+		djset.MakeSet(i)
+		if w > maxWeight {
+			maxWeight = w
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		var dst, src int
+		fmt.Scanf("%v %v", &dst, &src)
+		l := djset.Find(dst)
+		r := djset.Find(src)
+
+		if l != r {
+			weights[l], weights[r] = weights[l] + weights[r], weights[l] + weights[r]
+			djset.Union(l, r)
+			if weights[l] > maxWeight {
+				maxWeight = weights[l]
+			}
+		}
+
+		fmt.Println(maxWeight)
+	}
+}
