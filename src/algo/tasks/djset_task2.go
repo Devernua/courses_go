01caa4446ee618c02b659165475309eba41f365f
a.kukuev <a.kukuev@corp.mail.ru>
2018-06-03 13:12:57 +0300

    impl second task for djset

--- src/algo/tasks/djset_task2.go
+++ src/algo/tasks/djset_task2.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+	"algo/disjoinset"
+)
+
+func main() {
+	result := 1
+	var n, e, d int
+	fmt.Scanf("%v %v %v", &n, &e, &d)
+
+	djset := disjoinset.NewDisJoinSet(n + 1)
+
+	for i := 1; i <= n; i++ {
+		djset.MakeSet(i)
+	}
+
+	for i := 0; i < e; i++ {
+		var dst, src int
+		fmt.Scanf("%v %v", &dst, &src)
+		djset.Union(dst, src)
+	}
+
+	for i := 0; i < d; i++ {
+		var dst, src int
+		fmt.Scanf("%v %v", &dst, &src)
+		if djset.Find(dst) == djset.Find(src) {
+			result = 0
+		}
+	}
+
+	fmt.Println(result)
+}
