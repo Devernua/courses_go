1e7402ca938e6ae988f5e6833cd1ea15f64db11b
a.kukuev <a.kukuev@corp.mail.ru>
2018-06-13 09:16:32 +0300

    little more pretty execute piple for coursera task2

--- src/coursera/hw2_signer/signer.go
+++ src/coursera/hw2_signer/signer.go
@@ -4,24 +4,22 @@ import "sync"
 
 func startTask(task job, in, out chan interface{}, wg *sync.WaitGroup) {
 	defer wg.Done()
+	defer close(out)
+
 	task(in, out)
-	close(out)
 }
 
 // сюда писать код
 func ExecutePipeline(tasks ...job) {
 	wg := &sync.WaitGroup{}
 
-	var curIn chan interface{}
-	curOut := make(chan interface{})
-	for idx, task := range tasks {
+	var curIn, curOut chan interface{}
+	for _, task := range tasks {
+		curIn = curOut
+		curOut = make(chan interface{})
+
 		wg.Add(1)
 		go startTask(task, curIn, curOut, wg)
-
-		if idx + 1 != len(tasks) {
-			curIn = curOut
-			curOut = make(chan interface{})
-		}
 	}
 
 	wg.Wait()
