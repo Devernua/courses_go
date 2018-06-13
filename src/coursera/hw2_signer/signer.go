7b58f05927e8ff7c91b2e4f3bba7aa504dcf9305
a.kukuev <a.kukuev@corp.mail.ru>
2018-06-13 09:11:49 +0300

    impl execute pipeline for coursera task2

--- src/coursera/hw2_signer/signer.go
+++ src/coursera/hw2_signer/signer.go
@@ -1,3 +1,40 @@
 package main
 
+import "sync"
+
+func startTask(task job, in, out chan interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	task(in, out)
+	close(out)
+}
+
 // сюда писать код
+func ExecutePipeline(tasks ...job) {
+	wg := &sync.WaitGroup{}
+
+	var curIn chan interface{}
+	curOut := make(chan interface{})
+	for idx, task := range tasks {
+		wg.Add(1)
+		go startTask(task, curIn, curOut, wg)
+
+		if idx + 1 != len(tasks) {
+			curIn = curOut
+			curOut = make(chan interface{})
+		}
+	}
+
+	wg.Wait()
+}
+
+func SingleHash(in, out chan interface{}) {
+
+}
+
+func MultiHash(in, out chan interface{}) {
+
+}
+
+func CombineResults(in, out chan interface{}) {
+
+}
