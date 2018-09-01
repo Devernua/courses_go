1a089d3cc40dfc0522adc58c1d384de826a5841f
Ann <unbeman@gmail.com>
2018-09-01 18:11:00 +0300

    add easyjson and prints

--- src/lang/task_on_bench/fast.go
+++ src/lang/task_on_bench/fast.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"github.com/mailru/easyjson"
+	"github.com/mailru/easyjson/jlexer"
+	"github.com/mailru/easyjson/jwriter"
 	"io"
 	"os"
 	"strings"
@@ -19,6 +22,91 @@ type User struct {
 	Phone string `json:"phone"`
 }
 
+// suppress unused package warning
+var (
+	_ *json.RawMessage
+	_ *jlexer.Lexer
+	_ *jwriter.Writer
+	_ easyjson.Marshaler
+)
+
+func easyjson2bc03518DecodeLangTaskOnBench(in *jlexer.Lexer, out *User) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeString()
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		case "company":
+			out.Company = string(in.String())
+		case "country":
+			out.Country = string(in.String())
+		case "email":
+			out.Email = string(in.String())
+		case "job":
+			out.Job = string(in.String())
+		case "name":
+			out.Name = string(in.String())
+		case "phone":
+			out.Phone = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson2bc03518DecodeLangTaskOnBench(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson2bc03518DecodeLangTaskOnBench(l, v)
+}
+
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
 	file, err := os.Open(filePath)
@@ -32,14 +120,15 @@ func FastSearch(out io.Writer) {
 
 	seenBrowsers := make(map[string]struct{})
 	uniqueBrowsers := 0
-	foundUsers := ""
 	i := -1
+	var user User
+
+	fmt.Fprintln(out, "found users:")
 
 	for scanner.Scan() {
 		i++
 
-		var user User
-		err := json.Unmarshal([]byte(scanner.Text()), &user)
+		err := easyjson.Unmarshal([]byte(scanner.Text()), &user)
 		if err != nil {
 			panic(err)
 		}
@@ -61,10 +150,10 @@ func FastSearch(out io.Writer) {
 		}
 		if hasAndroid && hasMSIE {
 			emailParts := strings.Split(user.Email, "@")
-			foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, strings.Join(emailParts, " [at] "))
+			fmt.Fprintf(out, "[%d] %s <%s>\n", i, user.Name, strings.Join(emailParts, " [at] "))
 		}
 	}
 
-	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "")
 	fmt.Fprintln(out, "Total unique browsers", uniqueBrowsers)
 }
