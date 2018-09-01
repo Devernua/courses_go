4c9fbafc9deeeb0b9ee1221ee61d162e3bbe3c62
Ann <unbeman@gmail.com>
2018-09-01 16:34:42 +0300

    more optimization on task for prof and bench

--- src/lang/task_on_bench/fast.go
+++ src/lang/task_on_bench/fast.go
@@ -1,10 +1,100 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
 	"io"
+	"os"
+	"regexp"
 )
+
+type User struct {
+	Browsers []string `json:"browsers"`
+	Company string `json:"company"`
+	Country string `json:"country"`
+	Email string `json:"email"`
+	Job string `json:"job"`
+	Name string `json:"name"`
+	Phone string `json:"phone"`
+}
 
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	r := regexp.MustCompile("@")
+	seenBrowsers := []string{}
+	uniqueBrowsers := 0
+	foundUsers := ""
+	i := -1
+
+	for scanner.Scan() {
+		i++
+
+		var user User
+		// fmt.Printf("%v %v\n", err, line)
+		err := json.Unmarshal([]byte(scanner.Text()), &user)
+		if err != nil {
+			panic(err)
+		}
+
+		isAndroid := false
+		isMSIE := false
+
+		for _, browser := range user.Browsers {
+
+			if ok, err := regexp.MatchString("Android", browser); ok && err == nil {
+				isAndroid = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		for _, browser := range user.Browsers {
+
+			if ok, err := regexp.MatchString("MSIE", browser); ok && err == nil {
+				isMSIE = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		// TODO: use in regexp BOTH
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		// log.Println("Android and MSIE user:", user["name"], user["email"])
+		email := r.ReplaceAllString(user.Email, " [at] ")
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email)
+	}
+
+	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
