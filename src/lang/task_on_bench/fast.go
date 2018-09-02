11081a1ddb942335016e34289556202b59d7baa6
Ann <unbeman@gmail.com>
2018-09-02 09:53:02 +0300

    faster

--- src/lang/task_on_bench/fast.go
+++ src/lang/task_on_bench/fast.go
@@ -14,12 +14,8 @@ import (
 
 type User struct {
 	Browsers []string `json:"browsers"`
-	Company string `json:"company"`
-	Country string `json:"country"`
 	Email string `json:"email"`
-	Job string `json:"job"`
 	Name string `json:"name"`
-	Phone string `json:"phone"`
 }
 
 // suppress unused package warning
@@ -72,18 +68,10 @@ func easyjson2bc03518DecodeLangTaskOnBench(in *jlexer.Lexer, out *User) {
 				}
 				in.Delim(']')
 			}
-		case "company":
-			out.Company = string(in.String())
-		case "country":
-			out.Country = string(in.String())
 		case "email":
 			out.Email = string(in.String())
-		case "job":
-			out.Job = string(in.String())
 		case "name":
 			out.Name = string(in.String())
-		case "phone":
-			out.Phone = string(in.String())
 		default:
 			in.SkipRecursive()
 		}
@@ -116,44 +104,44 @@ func FastSearch(out io.Writer) {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
 
 	seenBrowsers := make(map[string]struct{})
-	uniqueBrowsers := 0
 	i := -1
 	var user User
+	var tmp int
+	hasAndroid := false
+	hasMSIE := false
+	isAndroid := false
+	isMSIE := false
 
 	fmt.Fprintln(out, "found users:")
 
 	for scanner.Scan() {
 		i++
+		user.UnmarshalJSON([]byte(scanner.Text()))
 
-		err := easyjson.Unmarshal([]byte(scanner.Text()), &user)
-		if err != nil {
-			panic(err)
-		}
-
-		hasAndroid := false
-		hasMSIE := false
+		hasAndroid = false
+		hasMSIE = false
 		for _, browser := range user.Browsers {
-			isAndroid := strings.Contains(browser, "Android")
-			isMSIE := strings.Contains(browser, "MSIE")
+			isAndroid = strings.Contains(browser, "Android")
+			isMSIE = strings.Contains(browser, "MSIE")
 
 			if isAndroid || isMSIE {
 				if _, ok := seenBrowsers[browser]; !ok {
 					seenBrowsers[browser] = struct{}{}
-					uniqueBrowsers++
 				}
 				hasAndroid = isAndroid || hasAndroid
 				hasMSIE = isMSIE || hasMSIE
 			}
 		}
 		if hasAndroid && hasMSIE {
-			emailParts := strings.Split(user.Email, "@")
-			fmt.Fprintf(out, "[%d] %s <%s>\n", i, user.Name, strings.Join(emailParts, " [at] "))
+			tmp = strings.IndexAny(user.Email, "@")
+			fmt.Fprintf(out, "[%d] %s <%s [at] %s>\n", i, user.Name, user.Email[0:tmp], user.Email[tmp + 1: len(user.Email)])
+			//emailParts := strings.Split(user.Email, "@")
+			//fmt.Fprintf(out, "[%d] %s <%s>\n", i, user.Name, strings.Join(emailParts, " [at] "))
 		}
 	}
 
 	fmt.Fprintln(out, "")
-	fmt.Fprintln(out, "Total unique browsers", uniqueBrowsers)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
